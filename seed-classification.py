5ff5779d31cbb581089ae0085644b3a97358aaa3
Diego Roessle <=>
2019-05-17 16:33:35 -0300

    Acrescenta leitura de base de dados

--- seed-classification.py
+++ seed-classification.py
@@ -2,5 +2,55 @@ import cv2, os
 import numpy as np
 import matplotlib.pyplot as plt
 
+class DB:
+	data = []
+
+	def __init__(self, root_folder):
+		#lists directories
+		directory_list = list()
+		for root, dirs, files in os.walk(root_folder, topdown=False):
+			for name in dirs:
+				directory_list.append((name, os.path.join(root, name)))
+		#gets images from every listed directory
+		images = list()
+		for name, directory in directory_list:
+			images = self.load_images_from_folder(directory, name)
+			#appends images from folder to data
+			if len(images) > 0:
+				self.data = self.data + images	
+
+	def load_images_from_folder(self, folder, classification):
+		images = list()
+		files = os.listdir(folder)
+		for filename in files:
+			if any([filename.endswith(x) for x in ['.jpeg', '.jpg', '.JPG']]):
+				img = cv2.imread(os.path.join(folder, filename))
+				if img is not None:
+					image = Image(filename, classification, img)
+					images.append(image)
+		return images
+
+	def getData(self):
+		return self.data
+
+class Image:
+	image = ''
+	name = ''
+	classification = ''
+	features = ''
+
+	def __init__(self, name, classification, image):
+		self.image = image
+		self.name = name
+		self.classification = classification
 
 #main
+#----------------------------------------------------
+
+#Loads Database
+db = DB('./images')
+images = db.getData()
+
+#Printing test
+# for image in images:
+	# print(image.name, image.classification)
